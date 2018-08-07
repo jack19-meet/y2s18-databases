a5b98ffb848a7d99d85b5ea5726d4ef57a98be0d
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 12:12:58 +0300

    g

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -22,7 +22,7 @@ def query_article_by_topic(topic):
 	return knowledge
 
 def query_article_by_rating(threshold):
-	knowledge=session.query(Knowledge).filter(Knowledge.rate<threshold).all()
+	knowledge=session.query(Knowledge).filter(Knowledge.rating<threshold).all()
 
 def delete_article_by_topic(topic):
 	session.query(Knowledge).filter_by(topic=topic).delete()
@@ -33,11 +33,23 @@ def delete_all_articles():
 	session.commit()
 
 def edit_article_rating(update_rating,article_title):
-	art_rate=session.query(Knowledge).filter_by(title=article_title).first()
+	art_rate=session.query(Knowledge).filter_by(topic=article_title).first()
 	art_rate.rating=update_rating
 	session.commit()
+
+
+def delete_article_by_rating(threshold):
+	session.query(Knowledge).filter(Knowledge.rating<threshold).delete()
+	session.commit()
+
+def get_top_rated_5():
+	knowledge=query_all_articles()
+
 	
-#delete_all_articles()
+delete_all_articles()
 #add_article("birds", "birds are nice", 7)
-edit_article_rating(10,"birds")
-print(query_all_articles())	
+#add_article("idk", "idk, google it",4)
+#edit_article_rating(10,"birds")
+#delete_article_by_topic("birds")
+
+print(query_all_articles())		
